58552178be95a9ae67e813da1e81e0d46c2bd6d2
Adithya <adithya.bellary@gmail.com>
2020-02-09 00:25:32 -0600

    comment out project again in bable.config

--- emphasis_education_app/App.tsx
+++ emphasis_education_app/App.tsx
@@ -65,7 +65,7 @@ const App = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Learn More</Text>
               <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next!!!
+                Read the docs to discover what to do next DUDE
               </Text>
             </View>
             <LearnMoreLinks />
