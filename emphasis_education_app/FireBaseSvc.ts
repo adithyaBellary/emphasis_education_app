e68d669c32bef18a1ff74d4ea0a8b7b5bbdf2676
Adithya <adithya.bellary@gmail.com>
2020-03-29 22:21:41 -0500

    got basic messages to show

--- emphasis_education_app/FireBaseSvc.ts
+++ emphasis_education_app/FireBaseSvc.ts
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import uuid from 'uuid';
 
 import { firebaseConfig } from './config/firebase';
 
@@ -44,6 +45,82 @@ class FireBaseSvc {
     } else {
       console.log('reusing auth');
     }
+  }
+
+  // TODO type this shit
+  onLogout = user => {
+    firebase.auth().signOut().then(() => {
+      console.log('sign out is successful')
+    }).catch((e) => {
+      console.log('an error happened when signing out')
+    })
+  }
+
+  // figure out this uuid business
+  uid () {
+    const curUser = firebase.auth().currentUser;
+    if (!curUser) {
+      console.log('the current user is null');
+      // is this the right thing to be returning if the user is null?
+      return null;
+    } else {
+      return curUser.uid;
+    }
+  }
+
+  // _ref () {
+  //   return firebase.database().ref('Messages');
+  // }
+
+  _ref() {
+    return firebase.database().ref('Messages');
+  }
+
+  // TODO type this shit
+  parse = snapshot => {
+    const { timestamp: numberStamp, text, user } = snapshot.val();
+    const { key: id } = snapshot;
+    // needed for giftedChat
+    const { key: _id } = snapshot;
+    const timestamp = new Date(numberStamp);
+    // add typing here
+    const message = {
+      id,
+      _id,
+      timestamp,
+      text,
+      user
+    };
+
+    return message;
+  }
+
+  // need to know more about this function
+  refOn = callBack => {
+    this._ref()
+      .limitToLast(20)
+      .on('child_added', (snapshot: any) => callBack(this.parse(snapshot)))
+  }
+
+  timeStamp() {
+    return firebase.database.ServerValue.TIMESTAMP;
+  }
+
+  send = messages => {
+    messages.forEach(element => {
+      const { text, user } = element;
+      const message = {
+        text,
+        user,
+        createdAt: this.timeStamp
+      };
+      console.log('sending a message');
+      this._ref().push(message);
+    });
+  }
+
+  refOff () {
+    this._ref().off();
   }
 }
 
--- emphasis_education_app/src/components/Chat.tsx
+++ emphasis_education_app/src/components/Chat.tsx
@@ -1,11 +1,82 @@
 import React from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 
-class Chat extends React.Component {
+import firebaseSvc from '../../FireBaseSvc';
+
+interface IChatProps {
+  // TODO should the ID be of type number or ID
+  id: number;
+  name: string;
+  email: string;
+  subject: string;
+  navigation: any;
+}
+
+interface IChatState {
+  // this should be of type Message
+  messages: Array<any>;
+}
+
+const test_user = {
+  name: 'test user',
+  email: 'test@gmail.com'
+}
+class Chat extends React.Component<IChatProps, IChatState> {
+  constructor (props: IChatProps) {
+    super(props)
+  }
+
+  state = {
+    messages: []
+  };
+
+  // add hooks if youre real
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'this is a test message',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Test User',
+            avatar: 'https://placeimg.com/140/140/any'
+          }
+        },
+        {
+          _id: 2,
+          text: 'this is another test message',
+          createdAt: new Date(),
+          user: {
+            _id: 3,
+            name: 'diff Test User',
+            // avatar: 'https://placeimg.com/140/140/any'
+          }
+        }
+      ]
+    })
+  }
+
+  user() {
+    return {
+      name: this.props.name,
+      email: this.props.email,
+      id: firebaseSvc.uid(),
+      _id: firebaseSvc.uid()
+    };
+  }
 
   public render () {
     return (
-      <GiftedChat />
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={firebaseSvc.send}
+        user={{
+          _id: 1,
+          // name: 'another test'
+        }}
+      />
     )
   }
 }
