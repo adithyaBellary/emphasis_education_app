ac4909c0bfd37727311d2c0608144dd67e50b2d6
Adithya <adithya.bellary@gmail.com>
2020-05-25 01:14:20 -0500

    add typing to firebaseSvc

--- Server/firebaseSvc.ts
+++ Server/firebaseSvc.ts
@@ -6,8 +6,14 @@ import { MD5 } from "crypto-js"
 import pubsub from './pubsub';
 import { firebaseConfig } from './config/firebase';
 import { MESSAGE_RECEIVED_EVENT, NUM_FETCH_MESSAGES } from './constants';
-import { IMessage } from './types/IMessage';
-import { UserInfoType, MutationLoginArgs, MessageInput } from './types/schema-types';
+import {
+  UserInfoType,
+  MutationLoginArgs,
+  MessageInput,
+  LoginPayload,
+  MessageType,
+} from './types/schema-types';
+import { EventEmitter } from 'events';
 
 const MESSAGE_REF_BASE: string = 'Messages';
 const User_REF_BASE: string = 'Users';
@@ -21,9 +27,10 @@ class FireBaseSVC {
     this.test_listen();
   }
 
-  getChatId = async (user) => {
-    const hash: string = MD5(user.email).toString();
-    const curUser = await this.getUser(hash);
+  getChatId = async (email: string) => {
+    const hash: string = MD5(email).toString();
+    const curUser: UserInfoType = await this.getUser(hash);
+
     return curUser.chatIDs;
   }
 
@@ -38,12 +45,10 @@ class FireBaseSVC {
       () => res = true,
       () => res = false
     );
-    const chatIDs: string[] = await this.getChatId(user);
-    return {
-      res,
-      chatIDs
-    }
+    const chatIDs: string[] = await this.getChatId(user.email);
+    const payload: LoginPayload = { res, chatIDs };
 
+    return payload;
   }
 
   // observeAuth = () => {
@@ -179,26 +184,6 @@ class FireBaseSVC {
     await this._refFamily(groupID).push(user_and_id)
   }
 
-  // TODO type this shit
-  // define typing of the snapshot
-  parse = snapshot => {
-    const { timestamp: numberStamp, text, user } = snapshot.val();
-    const { key: id } = snapshot;
-    // needed for giftedChat
-    const { key: _id } = snapshot;
-    const timestamp = new Date(numberStamp);
-    // add typing here
-    const message = {
-      id,
-      _id,
-      timestamp,
-      text,
-      user
-    };
-
-    return message;
-  }
-
   // start at 0
   getMessages = async (chatID: string, init: number) => {
     const chatHash: string = MD5(chatID).toString();
@@ -223,7 +208,7 @@ class FireBaseSVC {
         const val = snap.val();
         // console.log('val', val)
         const key = Object.keys(val)
-        const mess = key.map(k => {
+        const mess: MessageType[] = key.map(k => {
           const {messageID, ...rest} = val[k];
           return {
             ...rest,
@@ -233,13 +218,6 @@ class FireBaseSVC {
         return mess
       })
   }
-
-  // need to know more about this function
-  // refOn = callBack => {
-  //   this._refMessage()
-  //     .limitToLast(20)
-  //     .on('value', (snapshot) => callBack(this.parse(snapshot)))
-  // }
 
   getRecentId = async (chatID: string) => {
     const chatHash: string = MD5(chatID).toString();
@@ -251,7 +229,6 @@ class FireBaseSVC {
         if (!val) { return 0 }
         console.log('val', val);
         const key = Object.keys(val);
-        console.log('length of keys: (should be 1)', key.length)
         const oldMessageID = val[key[0]].messageID;
         return oldMessageID - 1;
       })
@@ -261,10 +238,7 @@ class FireBaseSVC {
     console.log('listener is on')
     this._refMessage('')
     .on('child_changed', (snapshot) => {
-      // console.log('snapshot', snapshot.val())
-      // console.log('snapshot', snapshot.ref.key)
       const val = snapshot.val();
-      // get the last key
       const key = Object.keys(val).slice(-1)[0]
       pubsub.publish(MESSAGE_RECEIVED_EVENT, {
         messageReceived: {
@@ -283,10 +257,6 @@ class FireBaseSVC {
       .on('child_added', (snap) => {
         const snapVal = snap.val();
         const key = Object.keys(snapVal)[0];
-        // console.log('child added')
-        // console.log(snapVal)
-        // console.log(snap.ref.key)
-        // publish to the pubsub bring this out
         pubsub.publish(MESSAGE_RECEIVED_EVENT, {
           messageReceived: {
             MessageId: snapVal[key].messageID,
@@ -309,9 +279,6 @@ class FireBaseSVC {
         // if this is a new chat wont have any values yet
         if (!val) { return 0 }
         return val;
-        // const key = Object.keys(val)[0];
-        // const numMessages: number = val[key];
-        // return numMessages;
       })
   }
 
@@ -334,8 +301,6 @@ class FireBaseSVC {
     this.updateNumMessages(messages[0].chatID);
     messages.forEach(async (element: MessageInput) => {
       const { text, user, chatID } = element;
-      // console.log(text)
-      // myMesID = oldMess === 0 ? 0 : oldMess - 1;
       myMesID = oldMess;
       const message = {
         text,
