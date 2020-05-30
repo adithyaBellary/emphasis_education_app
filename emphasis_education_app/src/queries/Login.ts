4fc580fd170079a1cf73d18515d9936ac2c7b4de
Adithya <adithya.bellary@gmail.com>
2020-05-30 01:26:00 -0500

    update login query

--- emphasis_education_app/src/queries/Login.ts
+++ emphasis_education_app/src/queries/Login.ts
@@ -4,6 +4,12 @@ export const LOGIN = gql`
 mutation login($email: String!, $password: String!) {
   login(email: $email, password: $password) {
     res
+    name
+    email
+    phoneNumber
+    userType
+    groupID
+    _id
     chatIDs
   }
 }
