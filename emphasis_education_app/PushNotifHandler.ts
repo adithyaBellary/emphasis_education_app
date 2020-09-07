213c9671f44115410f506f2567b4095bcedf2943
Adithya <adithya.bellary@gmail.com>
2020-09-07 12:19:10 -0500

    add push notif configure

--- emphasis_education_app/PushNotifHandler.ts
+++ emphasis_education_app/PushNotifHandler.ts
@@ -1,3 +1,37 @@
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import PushNotification from 'react-native-push-notification';
 
+PushNotification.configure({
+  onRegister: token => {
+    console.log('on register,', token)
+  },
+
+  onNotification: notification => {
+    console.log('on notification, ', notification)
+
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  onAction: action => {
+    console.log('on action, ', action)
+  },
+
+  onRegistrationError: err => {
+    console.log('on reg error', err)
+  },
+
+  onRemoteFetch: remoteFetch => {
+    console.log('on remote fetch', remoteFetch)
+  },
+
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  popInitialNotification: true,
+
+  requestPermissions: true
+});
+
