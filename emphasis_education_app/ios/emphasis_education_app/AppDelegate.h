b6a983e2e635a50bcae96bface6997d06e410c16
Adithya <adithya.bellary@gmail.com>
2020-09-07 11:21:45 -0500

    update appdelegate.h

--- emphasis_education_app/ios/emphasis_education_app/AppDelegate.h
+++ emphasis_education_app/ios/emphasis_education_app/AppDelegate.h
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 #import <UserNotifications/UNUserNotificationCenter.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
