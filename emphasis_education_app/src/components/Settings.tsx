0e8089c1a4c5a8616dd57fd55267260195d84c66
Adithya <adithya.bellary@gmail.com>
2020-09-19 15:06:37 -0500

    add send bug email functionality

--- emphasis_education_app/src/components/Settings.tsx
+++ emphasis_education_app/src/components/Settings.tsx
@@ -2,10 +2,24 @@ import * as React from 'react';
 import {
   View,
   Text,
+  TextInput
 } from 'react-native';
+import styled from 'styled-components';
+import { useMutation } from '@apollo/react-hooks';
 
-import { ButtonContainer, MyButton, MyButtonText, ThemedButton } from './shared';
-import { AuthContext } from './Context/Context';
+import {
+  ThemedButton,
+  ContentContain as Contain
+} from './shared';
+import { AuthContext, GeneralContext } from './Context/Context';
+import { SEND_BUG_EMAIL } from '../queries/SendBugEmail';
+
+const StyledTextInput = styled(TextInput)`
+  height: 100px
+  border: grey solid 1px;
+  padding: 20px;
+  border-radius: 10px;
+`;
 
 interface SettingsProps {
   navigation: any;
@@ -13,25 +27,40 @@ interface SettingsProps {
 }
 
 const Settings: React.FC<SettingsProps> = () => {
-
+  const [message, setMessage] = React.useState('');
   const { logout } = React.useContext(AuthContext);
+  const { loggedUser } = React.useContext(GeneralContext);
+
+  const [runMutation, {data, loading}] = useMutation(SEND_BUG_EMAIL);
+
+  const sendEmail = () => {
+    runMutation({ variables: {
+      user: loggedUser.email,
+      body: message
+    }})
+  }
 
   return (
-    <View>
-      <Text>
+    <Contain>
+      {/* <Text>
         settings
-      </Text>
-      <Text>
-        add a section to get feedback / log a bug
-      </Text>
-      <ButtonContainer>
+      </Text> */}
+      <StyledTextInput
+        multiline={true}
+        placeholder='Notice anything weird? Let us know here!'
+        onChangeText={text => setMessage(text)}
+      />
+        <ThemedButton
+          buttonText='Submit'
+          loading={false}
+          onPress={() => sendEmail()}
+        />
         <ThemedButton
           buttonText='Logout'
           loading={false}
           onPress={() => logout()}
         />
-      </ButtonContainer>
-    </View>
+    </Contain>
   )
 }
 
