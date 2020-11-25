f1ae1cced323c7bc69c6a3d1c97c357654505ebf
Adithya <adithya.bellary@gmail.com>
2020-11-24 21:44:25 -0600

    bump version to 0.0.16

--- emphasis_education_app/src/components/Chat/Chat.tsx
+++ emphasis_education_app/src/components/Chat/Chat.tsx
@@ -107,47 +107,47 @@ const LiftedChat: React.FC<ChatProps> = ({ navigation, route }) => {
       // console.log('got data', data)
       console.log('got data in the sub!')
 
-      Sentry.captureMessage('Message received sub got data', {
-        user: {
-          email: loggedUser.email,
-          username: `${loggedUser.firstName} ${loggedUser.lastName}`
-        }
-      })
+      // Sentry.captureMessage('Message received sub got data', {
+      //   user: {
+      //     email: loggedUser.email,
+      //     username: `${loggedUser.firstName} ${loggedUser.lastName}`
+      //   }
+      // })
     },
   })
 
   if (subError) {
-    console.log('error in subscription', subError)
-    Sentry.captureException(subError, {
-      user: {
-        email: loggedUser.email,
-        error: subError
-      }
-    })
-    Sentry.captureMessage(subError.toString(), {
-      user: {
-        email: loggedUser.email,
-        error: subError
-      }
-    })
+    // console.log('error in subscription', subError)
+    // Sentry.captureException(subError, {
+    //   user: {
+    //     email: loggedUser.email,
+    //     error: subError
+    //   }
+    // })
+    // Sentry.captureMessage(subError.toString(), {
+    //   user: {
+    //     email: loggedUser.email,
+    //     error: subError
+    //   }
+    // })
   }
 
   if (subLoading) {
     console.log('subscription is loading')
-    Sentry.captureMessage('Subscription is loading', {
-      user: {
-        email: loggedUser.email
-      }
-    })
+    // Sentry.captureMessage('Subscription is loading', {
+    //   user: {
+    //     email: loggedUser.email
+    //   }
+    // })
   }
 
   if (!subLoading) {
     console.log('subscription is done loading')
-    Sentry.captureMessage('Subscription is done loading', {
-      user: {
-        email: loggedUser.email
-      }
-    })
+    // Sentry.captureMessage('Subscription is done loading', {
+    //   user: {
+    //     email: loggedUser.email
+    //   }
+    // })
   }
 
   const isLoading = queryLoading;
@@ -186,12 +186,12 @@ const LiftedChat: React.FC<ChatProps> = ({ navigation, route }) => {
       createdAt: new Date(subData.messageReceived.createdAt)
     }
 
-    Sentry.captureMessage('Received a message in chat', {
-      user: {
-        email: loggedUser.email,
-        username: `${loggedUser.firstName} ${loggedUser.lastName}`
-      }
-    })
+    // Sentry.captureMessage('Received a message in chat', {
+    //   user: {
+    //     email: loggedUser.email,
+    //     username: `${loggedUser.firstName} ${loggedUser.lastName}`
+    //   }
+    // })
 
     if (!curState ) {
       messages = [receivedMessage]
@@ -275,24 +275,24 @@ const LiftedChat: React.FC<ChatProps> = ({ navigation, route }) => {
           messages={curState ? curState.messages : []}
           triggerSubToMore={() => {
             console.log('subbing to more')
-            Sentry.captureMessage('triggered the subscribe to more stuff. should happen on mount', {
-              user: {
-                email: loggedUser.email,
-                name: `${loggedUser.firstName} ${loggedUser.lastName}`
-              }
-            })
+            // Sentry.captureMessage('triggered the subscribe to more stuff. should happen on mount', {
+            //   user: {
+            //     email: loggedUser.email,
+            //     name: `${loggedUser.firstName} ${loggedUser.lastName}`
+            //   }
+            // })
             subscribeToMore({
               document: SUB,
               updateQuery: (prev, data) => {
-                console.log('prev in sub more', prev)
-                console.log('data in sub more', data)
+                // console.log('prev in sub more', prev)
+                // console.log('data in sub more', data)
 
-                Sentry.captureMessage('data received in the subscribe to more part.', {
-                  user: {
-                    email: loggedUser.email,
-                    name: `${loggedUser.firstName} ${loggedUser.lastName}`
-                  }
-                })
+                // Sentry.captureMessage('data received in the subscribe to more part.', {
+                //   user: {
+                //     email: loggedUser.email,
+                //     name: `${loggedUser.firstName} ${loggedUser.lastName}`
+                //   }
+                // })
                 return prev;
               }
             })
