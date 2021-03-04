ff0a55aa003707240c9d829cacfc84f848cede1f
Adithya <adithya.bellary@gmail.com>
2021-03-04 09:45:16 -0600

    bump to 1.0.0

--- emphasis_education_app/src/constant.ts
+++ emphasis_education_app/src/constant.ts
@@ -1,6 +1,6 @@
 import { Dimensions } from 'react-native';
 
-export const VERSION = '0.0.21'
+export const VERSION = '1.0.0'
 export const LOGIN_TOKEN: string = 'LOGIN';
 export const NOTIFICATIONS_KEY = 'NOTIFICATIONS';
 
